f85d72b94f48857f8ac956fe29ba56de582e0b81
Abrahão Martins <abrahaosrmartins@gmail.com>
2025-04-20 21:58:09 -0300

    style: retirando código inicialmente desnecessário

--- app/Domain/Wallet/Models/Wallet.php
+++ app/Domain/Wallet/Models/Wallet.php
@@ -1,14 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Domain\Wallet\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
-    use HasFactory;
-
     /**
      * The attributes that are mass assignable.
      *
