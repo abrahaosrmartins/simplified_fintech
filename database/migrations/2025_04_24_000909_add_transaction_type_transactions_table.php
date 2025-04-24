ab6769a5073d4d3e5f27e56acc6df184a601aa9d
Abrahão Martins <abrahaosrmartins@gmail.com>
2025-04-23 23:08:05 -0300

    chore: adicionando coluna de tipo de transação, no banco

---
+++ database/migrations/2025_04_24_000909_add_transaction_type_transactions_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Domain\Transaction\Enums\TransactionTypesEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->enum('type', [TransactionTypesEnum::values()])->after('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropColumn('type');
+    }
+};
