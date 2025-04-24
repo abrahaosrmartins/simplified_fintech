2529b1d37fd25bf27fd23590e7ea9c279f5a72d4
Abrahão Martins <abrahaosrmartins@gmail.com>
2025-04-23 22:52:32 -0300

    refactor: Adicionando campos no inputdto de transação

--- app/Application/Transaction/UseCases/Dto/TransactionInputDto.php
+++ app/Application/Transaction/UseCases/Dto/TransactionInputDto.php
@@ -7,14 +7,17 @@ class TransactionInputDto
     public float $value;
     public int $payer;
     public int $payee;
+    public string $type;
 
     public function __construct(
         float $value,
         int $payer,
-        int $payee
+        int $payee,
+        string $type,
     ) {
         $this->value = $value;
         $this->payer = $payer;
         $this->payee = $payee;
+        $this->type = $type;
     }
 }
