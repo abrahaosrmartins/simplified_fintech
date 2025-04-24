cfd0befcbc241bd6936708348f9ad9fcd529e4cf
Abrahão Martins <abrahaosrmartins@gmail.com>
2025-04-23 22:51:15 -0300

    refactor: Atualizando caso de uso de criação de transação
    - Retiradas importações desnecessárias
    - Reimplementada autorização externa
    - Adicionada regra de negócio de transferência para si mesmo
    - Mensagens de erro atualizadas

--- app/Application/Transaction/UseCases/CreateTransactionUseCase.php
+++ app/Application/Transaction/UseCases/CreateTransactionUseCase.php
@@ -4,49 +4,48 @@ namespace App\Application\Transaction\UseCases;
 
 use App\Application\Transaction\UseCases\Dto\TransactionInputDto;
 use App\Domain\Transaction\Repositories\TransactionRepositoryInterface;
-use App\Domain\TransactionWasApproved;
+use App\Domain\TransactionStatusUpdated;
 use App\Domain\User\Enums\UserTypeEnum;
 use App\Domain\User\Models\User;
 use App\Domain\Wallet\Repositories\WalletRepositoryInterface;
 use App\Services\External\Contracts\AuthorizerServiceInterface;
-use App\Services\External\Contracts\NotificationServiceInterface;
 use Exception;
 
 class CreateTransactionUseCase
 {
     private AuthorizerServiceInterface $authorizeService;
     private TransactionRepositoryInterface $transactionRepository;
-    private NotificationServiceInterface $notificationService;
     private WalletRepositoryInterface $walletRepository;
 
     public function __construct(
         AuthorizerServiceInterface $authorizeService,
-        NotificationServiceInterface $notificationService,
         TransactionRepositoryInterface $transactionRepository,
         WalletRepositoryInterface $walletRepository
     ) {
         $this->authorizeService = $authorizeService;
         $this->transactionRepository = $transactionRepository;
-        $this->notificationService = $notificationService;
         $this->walletRepository = $walletRepository;
     }
 
     public function execute(User $user, TransactionInputDto $transactionInputDto)
     {
-        $this->validatesUserCanMakeTransfer($user);
+        $this->validatesUserCanMakeTransfer($user, $transactionInputDto->payee);
         $this->validatesUserHasBalance($user, $transactionInputDto->value);
-        // $this->executesExternalValidation();
+        $this->executesExternalValidation();
         $transaction = $this->persistTransaction($transactionInputDto);
-        event(new TransactionWasApproved($transaction));
-        $this->notificationService->notify($user->id);
+        event(new TransactionStatusUpdated($transaction));
 
         return $transaction;
     }
 
-    private function validatesUserCanMakeTransfer($user)
+    private function validatesUserCanMakeTransfer($user, $payee)
     {
         if ($user->type == UserTypeEnum::MERCHANT) {
-            throw new Exception('Oops! Você não pode realizar essa ação!');
+            throw new Exception('Oops! Usuários lojistas não podem fazer transferências.');
+        }
+
+        if ($user->id == $payee) {
+            throw new Exception('Oops! Você não pode transferir saldo para você mesmo!');
         }
 
         return true;
@@ -56,7 +55,7 @@ class CreateTransactionUseCase
         $wallet = $this->walletRepository->findByUserId($user->id);
 
         if (!$wallet || $wallet->balance < $value) {
-            throw new Exception('Você não possui saldo suficiente.');
+            throw new Exception('Oops! Você não possui saldo suficiente. :(');
         }
 
         return true;
@@ -76,7 +75,7 @@ class CreateTransactionUseCase
         $transaction = $this->transactionRepository->store($input);
 
         if (!$transaction) {
-            throw new Exception('Oops! Não foi possível executar estar ação. Por favor tente mais tarde ou contacte o administrado do sistema');
+            throw new Exception('Oops! Não foi possível executar estar ação. Por favor tente mais tarde ou contacte a administração do sistema');
         }
 
         return $transaction;
